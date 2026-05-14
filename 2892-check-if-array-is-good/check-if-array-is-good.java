898d5006dc31e39102cca2be3202e50f77cfa52b
Shankar Soni <soniyashwant282@gmail.com>
2026-05-14 19:01:27 +0530

    Time: 7 ms (15.24%) | Memory: 44.8 MB (59.43%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isGood(int[] nums) {
         Arrays.sort(nums);
-        
         int n = nums.length;
         if(nums[n-1]+1!=n) return false;
         int temp = nums[n-1];
         for(int i = 0; i < n; i++){
-            if(i < n-2){
-                if(nums[i]==nums[i+1]) return false;
+            if(i < n-2 && nums[i]==nums[i+1]){
+                 return false;
             }else{
                 if(nums[i]==temp) return true;
             }

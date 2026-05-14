0f1f8fc73848e1e5e57a37e2873a7e13fc766f90
Shankar Soni <soniyashwant282@gmail.com>
2026-05-14 08:48:26 +0530

    Time: 25 ms (25.22%) | Memory: 113.8 MB (78.22%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks,(a,b) -> (b[1] - b[0]) - (a[1] - a[0]));
+        int minEnergy = 0;
+        int currentEnergy = 0;
+        for(int i = 0; i < tasks.length; i++){
+            if(currentEnergy < tasks[i][1]){
+                minEnergy+=(tasks[i][1] - currentEnergy);
+                currentEnergy+=(tasks[i][1] - currentEnergy);
+            }
+            currentEnergy-=tasks[i][0];
+            // int req = tasks[i][1] - tasks[i][0];
+            // minEnegy+=tasks[i][1];
+            // if(tasks[i+1][1] > req){
+            //     minEnergy+=(tasks[i+1][1]-req);
+            //     currentEnergy+=()
+            // }
+        }
+        return minEnergy;
+    }
+}

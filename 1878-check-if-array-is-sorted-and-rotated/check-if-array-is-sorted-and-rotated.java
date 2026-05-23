f97f77c2a89589aab62d7c739516b62679854fed
Shankar Soni <soniyashwant282@gmail.com>
2026-05-23 09:55:11 +0530

    Time: 1 ms (5.67%) | Memory: 42.4 MB (99.26%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -6,8 +6,11 @@ class Solution {
         if(nums[0] < nums[n-1]) great++;
         for(int i = 0 ; i < n-1; i++){
            if(nums[i] > nums[i+1]) great++;
+           if(great>1){
+            return false;
+           }
         }
         System.out.print(great);
-        return great<=1;
+        return true;
     }
 }

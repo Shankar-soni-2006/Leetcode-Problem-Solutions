a235356fb1e6055b1b06d95c34889b0b38089866
Shankar Soni <soniyashwant282@gmail.com>
2026-05-26 18:20:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (94.30%) - LeetSync

--- 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.java
+++ 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.java
@@ -1,20 +1,26 @@
 class Solution {
     public String greatestLetter(String s) {
-        List<Character> great = new ArrayList<>();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
-                char upper = (char)(s.charAt(i)-32);
-                if(s.contains(""+upper) && !great.contains(upper)) great.add(upper);    
-            }else{
-                char lower = (char)(s.charAt(i)+32);
-                if(s.contains(""+lower)  && !great.contains(s.charAt(i))) great.add(s.charAt(i));    
+        for(char ch = 'Z'; ch >= 'A'; ch--){
+            if(s.indexOf(ch) != -1 && s.indexOf((char)(ch + 32)) != -1){
+                return String.valueOf(ch);
             }
         }
-        if(great.isEmpty()) return "";
-        char max = great.get(0);
-        for(int i = 1; i < great.size(); i++){
-           if(great.get(i) > max) max = great.get(i);
-        }
-        return ""+max;
+        return "";
+        // List<Character> great = new ArrayList<>();
+        // for(int i = 0; i < s.length(); i++){
+        //     if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+        //         char upper = (char)(s.charAt(i)-32);
+        //         if(s.contains(""+upper) && !great.contains(upper)) great.add(upper);    
+        //     }else{
+        //         char lower = (char)(s.charAt(i)+32);
+        //         if(s.contains(""+lower)  && !great.contains(s.charAt(i))) great.add(s.charAt(i));    
+        //     }
+        // }
+        // if(great.isEmpty()) return "";
+        // char max = great.get(0);
+        // for(int i = 1; i < great.size(); i++){
+        //    if(great.get(i) > max) max = great.get(i);
+        // }
+        // return ""+max;
     }
 }

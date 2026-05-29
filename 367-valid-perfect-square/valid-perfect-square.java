8d333b84aab24d7a08304e6e66ac1b220118a551
Shankar Soni <soniyashwant282@gmail.com>
2026-05-29 12:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (85.39%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,13 +1,19 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if (num < 1) return false;
-        
-        for (long  i = 1; i * i <= num; i++) { 
-            if (i * i == num) {
-                return true;
-            }
+        // if (num < 1) return false;
+        // for (long  i = 1; i * i <= num; i++) { 
+        //     if (i * i == num) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+        long l = 0, h = num;
+        while(l <= h){
+            long m = l+(h-l)/2;
+            if((m*m) == num) return true;
+            else if((m*m) < num) l = m+1;
+            else h = m-1;
         }
-        
         return false;
     }
 }

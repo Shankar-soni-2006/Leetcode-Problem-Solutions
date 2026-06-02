942e604b832e913effef84ad2e5a35bf6499e0c2
Shankar Soni <soniyashwant282@gmail.com>
2026-06-02 17:58:47 +0530

    Time: 10 ms (3.97%) | Memory: 47.1 MB (9.92%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int earliestFinishTime(int[] lST, int[] lD, int[] wST, int[] wD) {
+        int lmin = Integer.MAX_VALUE;
+        int ln = lST.length;
+        for(int i = 0 ; i < ln; i++) lmin = Math.min(lmin,(lST[i] +lD[i]));
+
+        int wMin = Integer.MAX_VALUE;
+        int Wn = wST.length;
+        for(int i = 0 ; i < Wn; i++) wMin = Math.min(wMin,(wST[i] + wD[i]));
+        
+        int minTime = Integer.MAX_VALUE;
+        for(int i = 0; i < Wn; i++) minTime = Math.min(minTime,(Math.max(lmin,wST[i])+wD[i]));
+
+        for(int i = 0; i < ln; i++) minTime = Math.min(minTime,(Math.max(wMin,lST[i])+lD[i]));
+        
+        System.out.print(lmin+" "+wMin+" "+minTime);
+        return minTime;
+    }
+}

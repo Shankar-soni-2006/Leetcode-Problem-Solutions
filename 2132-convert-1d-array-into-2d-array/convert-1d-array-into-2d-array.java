0e7088cd2002a511b0062c28b9bee6e1088ecc34
Shankar Soni <soniyashwant282@gmail.com>
2026-06-03 08:00:36 +0530

    Time: 5 ms (19.71%) | Memory: 67.1 MB (41.58%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -4,11 +4,14 @@ class Solution {
             return new int[0][0];
         }
         int[][] ans =  new int[m][n];
-        for(int i =0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans[i][j] = original[i*n+j];
-            }
+        for(int i = 0; i < original.length; i++){
+            ans[i/n][i%n] = original[i];
         }
+        // for(int i =0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         ans[i][j] = original[i*n+j];
+        //     }
+        // }
         return ans;
     }
 }

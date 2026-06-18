a506184a851e4dce285c78290485f01ef795f5cb
Shankar Soni <soniyashwant282@gmail.com>
2026-06-18 10:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (81.39%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,47 +1,31 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // Method 1
-        // int s = 0, n = nums.length, e = n-1;
-        // int peak = 0;
-        // while(s <= e){
-        //     int mid = s+(e-s)/2;
-        //     if(nums[0] <= nums[mid]){
-        //       peak = mid;
-        //       s = mid+1;  
-        //     }else e = mid - 1;
-        // }
-        // s = 0;
-        // e = peak;
-        // while(s <= e){
-        //     int mid = s+(e-s)/2;
-        //     if(nums[mid] == target) return mid;
-        //     else if(nums[mid] < target) s = mid+1;
-        //     else e = mid-1;
-        // }
-        // s = peak+1; 
-        // e = n-1;
-        // while(s <= e){
-        //     int mid = s+(e-s)/2;
-        //     if(nums[mid] == target) return mid;
-        //     else if(nums[mid] < target) s = mid+1;
-        //     else e = mid-1;
-        // }
-        // return -1;
-
-        //Method 2
-        int s = 0;
-        int e = nums.length-1;
-        while(s <= e){
-            int mid = s+(e-s)/2;
+        int peak = 0;
+        int n = nums.length;
+        int l = 0, h = n-1;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            if(nums[0] <= nums[mid]){
+                peak = mid;
+                l = mid+1;
+            }   else h = mid-1;          
+        }
+        l = 0;
+        h = peak;
+        while(l <= h){
+            int mid = l + (h -l)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[s] <= nums[mid]){ // first half
-                 if(target >= nums[s] && target < nums[mid]) e = mid -1;
-                 else s = mid+1;
-            }else{
-                if(target <= nums[e] && target > nums[mid]) s = mid+1;
-                else e = mid-1;
-            }
+            else if(nums[mid] < target) l  = mid+1;
+            else h = mid-1; 
+        }
+        l = peak+1;
+        h = n-1;
+        while(l <= h){
+            int mid = l + (h - l)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) l  = mid+1;
+            else h = mid-1; 
         }
         return -1;
     }
-} 
+}

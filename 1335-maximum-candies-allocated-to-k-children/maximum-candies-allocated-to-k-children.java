7bed0d6d318571300c8314c155db041be1cd5e62
Shankar Soni <soniyashwant282@gmail.com>
2026-06-18 14:58:51 +0530

    Time: 55 ms (6.55%) | Memory: 92 MB (67.66%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,23 +1,31 @@
 class Solution {
+    public boolean isPossible(int[] nums, long k, long mid){
+        long curr = 0;
+        for(int i = 0; i < nums.length; i++){
+            curr+=(nums[i]/mid);
+        }
+        if(curr >= k) return false;
+        return true;
+    }
     public int maximumCandies(int[] candies, long k) {
        int n = candies.length;
-       long low = 0, high = Integer.MAX_VALUE;
-       long  ans = 0;
+       long low = 1, high = Integer.MAX_VALUE;
+    //    long  ans = 0;
        while(low <= high){
            long maxCandy = low+(high-low)/2;
-           long currentCandy = 0;
-           if(maxCandy==0) break;
-           for(int i = 0 ; i < n; i++){
-                currentCandy+=(candies[i]/maxCandy);
-           }
-           if(currentCandy < k ){
-                // low = maxCandy+1;
-                high = maxCandy-1;
-           }else{
-                ans = maxCandy;
-              low = maxCandy+1;
-            }
+           if(isPossible(candies,k,maxCandy)) high = maxCandy-1;
+           else low = maxCandy+1;
+        //    long currentCandy = 0;
+        //    for(int i = 0 ; i < n; i++){
+        //         currentCandy+=(candies[i]/maxCandy);
+        //    }
+        //    if(currentCandy >= k){
+        //         ans = maxCandy;
+        //         low = maxCandy+1;
+        //    }else{
+        //         high = maxCandy-1;
+        //     }
        }
-        return (int)ans;
+        return (int)high;
     }
 }
